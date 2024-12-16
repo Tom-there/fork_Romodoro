3a2f62c243e7734d0cfff958f63482d2b5510058
D.J. Blume <dj.blume@fivetooldev.com>
2024-12-15 21:12:16 -0600

    Add basic timer functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::ffi::c_float;
+use std::io::Write;
+use std::thread::sleep;
+use std::time::Duration;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    time: c_float
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    
+    let duration_seconds = (args.time * 60.0).round() as i32;
+    
+
+    for remaining in (0..duration_seconds).rev() {
+        let minutes = remaining / 60;
+        let seconds = remaining % 60;
+
+        println!("\rTime Left: {:02}:{:02}", minutes, seconds);
+        std::io::stdout().flush().unwrap();
+
+        sleep(Duration::from_secs(1));
+    }
+
+    println!("\nTime's Up! Take a break!");
 }
